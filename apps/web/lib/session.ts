c4c10928b3c24a263d16da354bbf37ff0bfda0f9
mjk134 <mohitkjena@outlook.com>
2024-11-10 21:31:14 +0000

    feat: Add session

---
+++ apps/web/lib/session.ts
@@ -0,0 +1,164 @@
+import { cookies } from "next/headers";
+import db from "./database";
+
+export async function createSession(id: string) {
+  const currentSession = await getSession();
+  if (currentSession) return;
+
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  // 1. Create a session in the database
+  const sessionData = await db.session.create({
+    data: {
+      user_id: id,
+      expires: expiresAt,
+    },
+  });
+
+  const sessionId = sessionData.token;
+
+  // Create session using JWT using jose
+  const session = {
+    id: sessionId,
+    expires: expiresAt.toISOString(),
+    userId: id,
+  };
+
+  // 2. Sign the session token
+  const token = Buffer.from(JSON.stringify(session)).toString("base64");
+
+  // 3. Store the session in cookies for optimistic auth checks
+  (await cookies()).set("snowflake", token, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+type Session = {
+  id: string;
+  expires: string;
+  userId: string;
+};
+
+export const getSession = async (): Promise<Session | null> => {
+  const token = (await cookies()).get("snowflake");
+  if (!token) return null;
+
+  try {
+    const session = JSON.parse(
+      Buffer.from(token.value, "base64").toString("utf-8"),
+    );
+
+    return session;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getVerfiedSession = async () => {
+  const session = await getSession();
+
+  if (!session) return null;
+
+  const data = await db.session.findUnique({
+    where: {
+      token: (session as Record<string, any>).id,
+    },
+  });
+
+  if (!data) {
+    return null;
+  }
+  return session;
+};
+
+export async function getUserFromSession() {
+  const session = await getVerfiedSession();
+
+  if (!session) return null;
+
+  const user = await db.users.findUnique({
+    where: {
+      user_id: (session as Record<string, any>).userId,
+    },
+  });
+
+  return user;
+}
+
+export async function updateSession() {
+  const token = (await cookies()).get("snowflake");
+
+  if (!token) return null;
+
+  try {
+    const session = JSON.parse(
+      Buffer.from(token.value, "base64").toString("utf-8"),
+    );
+
+    // Update the session in the database
+    await db.session.update({
+      where: {
+        token: (session as Record<string, any>).id,
+      },
+      data: {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
+    });
+
+    const newSession = {
+      id: (session as Record<string, any>).id,
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(),
+      userId: (session as Record<string, any>).userId,
+    };
+
+    // Sign the session token
+
+    const newToken = Buffer.from(JSON.stringify(newSession)).toString("base64");
+
+    // Store the session in cookies for optimistic auth checks
+    (await cookies()).set("snowflake", newToken, {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      sameSite: "lax",
+      path: "/",
+    });
+
+    return newSession;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function deleteSession() {
+  const token = (await cookies()).get("snowflake");
+
+  if (!token) return;
+
+  try {
+    const session = JSON.parse(
+      Buffer.from(token.value, "base64").toString("utf-8"),
+    );
+
+    await db.session.delete({
+      where: {
+        token: (session as Record<string, any>).id,
+      },
+    });
+
+    (await cookies()).set("snowflake", "", {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(0),
+      sameSite: "lax",
+      path: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
